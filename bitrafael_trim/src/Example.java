8f4e566be9f023783978737788ec5691b6895c6c
b00lean <b00lean@b00lean.net>
2016-10-24 10:24:09 +0200

    Shrinked bitcoinj library and project(trim) that is capable of shrinking bitcoinj - needed for android projects (to avoid multidexing)

---
+++ bitrafael_trim/src/Example.java
@@ -0,0 +1,10 @@
+public class Example {
+    public static void main(String[] args) {
+        org.bitcoinj.params.MainNetParams x1 = null;
+        org.bitcoinj.core.DumpedPrivateKey x2 = null;
+        org.bitcoinj.core.Address x3 = null;
+        org.bitcoinj.core.Utils x4 = null;
+        org.bitcoinj.core.Sha256Hash x5 = null;
+        org.bitcoinj.core.Coin x6 = null;
+    }
+}
